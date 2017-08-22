1ae78c3ae0dc33ce58ac5f13d663012e954bbe9c
Jim <b10323024@yuntech.edu.tw>
2017-08-22 16:24:55 +0800

    add message of toast

--- app/src/main/java/com/example/haruna/gittest2/MainActivity.java
+++ app/src/main/java/com/example/haruna/gittest2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.haruna.gittest2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"123", Toast.LENGTH_LONG).show();
     }
 }
